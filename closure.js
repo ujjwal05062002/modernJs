039cf02f54f9c5886819e08abc99e9d6a5a2f5bc
ujjwal05062002 <ujjwal05062002@gmail.com>
2025-02-20 17:28:24 +0530

    added closure

---
+++ closure.js
@@ -0,0 +1,14 @@
+var a = 10;
+
+function outer(){
+    var b = 20
+    function inner(){
+        var c = 30
+    }
+    console.log(`in inner function a =${a} b = ${b}`);
+    return inner
+}
+
+var closure  = outer()  // closure stores the value of a and b and c even if it is not inside the function
+
+console.log(`${closure}`);
